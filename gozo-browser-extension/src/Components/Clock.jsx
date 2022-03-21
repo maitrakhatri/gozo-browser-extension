76c61e9d2895fbc75053cb0174f4fa53394757cf
Maitra Khatri <maitrakhatri@gmail.com>
2022-03-21 13:58:02 +0530

    components DONE

---
+++ gozo-browser-extension/src/Components/Clock.jsx
@@ -0,0 +1,5 @@
+export function Clock() {
+    return <div class="clock">
+        <h1>10:29</h1>
+    </div>;
+}
---
+++ gozo-browser-extension/src/Components/Greetings.jsx
@@ -0,0 +1,5 @@
+export function Greetings() {
+    return <div class="greeting">
+        <h1>Good Morning, Maitra !!</h1>
+    </div>;
+}
