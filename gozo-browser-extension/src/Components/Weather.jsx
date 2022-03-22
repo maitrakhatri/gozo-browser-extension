818cae08a6567efd6e7ecf2101fb3882507f2dfa
Maitra Khatri <maitrakhatri@gmail.com>
2022-03-22 20:35:06 +0530

    feat: Weather update

--- gozo-browser-extension/src/Components/Weather.jsx
+++ gozo-browser-extension/src/Components/Weather.jsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
+import { API_key } from "../apikey";
+
 export function Weather() {
+
+    const [temp, setTemp] = useState("")
+    const [location, setLocation] = useState("")
+    const [icon, setIcon] = useState("")
+
+    const [lat, setLat] = useState()
+    const [lon, setLon] = useState()
+
+    // Getting and Setting Live Latitude and Longitude Coordinates
+    navigator.geolocation.getCurrentPosition((position) => {
+        setLat(position.coords.latitude)
+        setLon(position.coords.longitude)
+    })
+
+    // Fetching and Setting Weather data
+    useEffect(() => {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}`).then((res) => res.json()).then((data) => {
+
+            const {name, main, weather} = data
+            setLocation(name);
+            setTemp(Math.round((main.temp)-273.15));
+            setIcon(weather[0].icon);
+        })
+    },[lat, lon, location, icon])
+        
     return <div class="weather">
-        <div class="icon"><img src="../../assets/sun.svg" alt="sun icon" /></div>
-        <div class="temp">32<span>&#176;</span></div>
-        <div class="loca">Ahmedabad</div>
+        <div class="icon"><img src={`https://openweathermap.org/img/wn/${icon}@2x.png`}/></div>
+        <div class="temp">{temp}<span>&#176;</span></div>
+        <div class="location">{location}</div>
     </div>;
 }
